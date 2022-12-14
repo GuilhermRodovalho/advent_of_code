6026cbdba102202cb354088fe2e7abc8b75ce107
Guilherme Rodovalho <guirodovalho10@gmail.com>
2022-12-13 22:09:45 -0300

    adding day6

---
+++ 2022/dia6/Program.cs
@@ -0,0 +1,60 @@
+﻿
+class TuningTrouble
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines("./input.txt");
+
+        foreach (string line in lines)
+        {
+            int result = GetFirstNonSubsequentCharacter(line);
+
+            Console.WriteLine($"Result = {result}");
+
+        }
+    }
+
+    private static int GetFirstNonSubsequentCharacter(string input)
+    {
+        Dictionary<char, int> amount = new Dictionary<char, int>();
+        int i = 0;
+        int j = 0;
+        while (i <= input.Length)
+        {
+            // Change this condition for part1 and two
+            if (i < 14)
+            {
+                if (!amount.TryAdd(input[i], 1))
+                {
+                    amount[input[i]]++;
+                }
+            }
+            else
+            {
+                if (!amount.TryAdd(input[i], 1))
+                {
+                    amount[input[i]]++;
+                }
+                amount[input[j]]--;
+
+                bool hasBiggerThanOne = false;
+                foreach (KeyValuePair<char, int> pair in amount)
+                {
+                    if (pair.Value > 1)
+                    {
+                        hasBiggerThanOne = true;
+                    }
+                }
+
+
+                if (!hasBiggerThanOne)
+                    return i + 1;
+
+                j++;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+}
