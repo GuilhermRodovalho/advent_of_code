60b86df8c3fa6e6f25260a8ab38b9796fb9919e9
Guilherme <guirodovalho10@gmail.com>
2024-04-25 15:20:41 -0300

    day2

---
+++ aoc2023/src/day1.rs
@@ -0,0 +1,113 @@
+// Função para criar e retornar o HashMap desejado
+fn create_number_map() -> Vec<(&'static str, u32)> {
+    vec![
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+}
+
+// Variável estática que armazena o HashMap retornado pela função
+fn get_first_number(line: &str) -> u32 {
+    let numbers = create_number_map();
+
+    for (i, c) in line.char_indices() {
+        if c.is_numeric() {
+            return c.to_digit(10).unwrap();
+        }
+        for (n, v) in &numbers {
+            if i + n.len() < line.len() {
+                if line[i..i + n.len()].contains(n) {
+                    return *v;
+                }
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+fn get_last_number(line: &str) -> u32 {
+    let numbers = create_number_map();
+
+    for (i, c) in line.char_indices().rev() {
+        if c.is_numeric() {
+            return c.to_digit(10).unwrap();
+        }
+        for (n, v) in &numbers {
+            if line[i..].contains(n) {
+                return *v;
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+pub fn solve_puzze(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|linha| {
+            let mut numero = String::new();
+
+            // acha o primeiro número
+            numero.push_str(get_first_number(linha).to_string().as_str());
+
+            // acha o último número
+            numero.push_str(get_last_number(linha).to_string().as_str());
+
+            numero
+        })
+        .fold(0, |acc, f| acc + f.parse::<i32>().unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn base_case() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+
+        assert_eq!(solve_puzze(input), 142)
+    }
+
+    #[test]
+    fn second_case() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        assert_eq!(solve_puzze(input), 281)
+    }
+
+    #[test]
+    fn test_get_first_number() {
+        assert_eq!(3, get_first_number("threeightfour"));
+        assert_eq!(1, get_first_number("1eighthree"));
+        assert_eq!(2, get_first_number("two1nine"));
+    }
+
+    // #[test]
+    // fn test_parse_str() {
+    //     let line = "two1nine";
+    //     assert_eq!("219", parse_str(&line));
+
+    //     assert_eq!("1ight", parse_str("oneight"));
+    //     assert_eq!("8wo3", parse_str("eightwothree"));
+    // }
+}
---
+++ aoc2023/src/day2.rs
@@ -0,0 +1,170 @@
+use std::collections::HashMap;
+
+fn get_colors_values_from_take(take: &str) -> [i32; 3] {
+    let mut colors_values = [0, 0, 0];
+    let _ = take
+        .split(",")
+        .map(|pair| {
+            let pair: Vec<&str> = pair.split_whitespace().collect();
+
+            match pair.get(1).unwrap() {
+                &"red" => {
+                    colors_values[0] = pair[0].parse().unwrap();
+                }
+                &"green" => {
+                    colors_values[1] = pair[0].parse().unwrap();
+                }
+                &"blue" => {
+                    colors_values[2] = pair[0].parse().unwrap();
+                }
+                _ => {}
+            }
+        })
+        .collect::<()>();
+
+    colors_values
+}
+
+fn get_largests_colors(line: &str) -> [i32; 3] {
+    let mut largest_colors = [0, 0, 0];
+    // remove the "Game N" part
+    let binding = line.split(":").collect::<Vec<&str>>();
+    let new_line = binding.get(1).unwrap();
+
+    let _ = new_line
+        .split(";")
+        .map(|take| {
+            let take_values = get_colors_values_from_take(take);
+            for (i, v) in take_values.iter().enumerate() {
+                if &largest_colors[i] < v {
+                    largest_colors[i] = *v
+                }
+            }
+        })
+        .collect::<()>();
+
+    largest_colors
+}
+
+fn calculate_power(values: [i32; 3]) -> i32 {
+    values[0] * values[1] * values[2]
+}
+
+fn is_valid_take(take: &str) -> bool {
+    let valid_takes = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+
+    let parts: Vec<&str> = take.split_whitespace().collect();
+    let quantity = parts.get(0).unwrap().parse::<i32>().unwrap();
+    let color = parts.get(1).unwrap();
+
+    !(valid_takes.get(color).unwrap() < &quantity)
+}
+
+fn solve_game_take(take: &str) -> bool {
+    take.split(",").all(|value| is_valid_take(value))
+}
+
+fn solve_game_line(line: &str) -> i32 {
+    let game_value = line
+        .split(":")
+        .next()
+        .unwrap()
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .get(1)
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    let games = line
+        .split(":")
+        .collect::<Vec<&str>>()
+        .get(1)
+        .unwrap()
+        .trim();
+
+    let is_valid_game = games
+        .split(";")
+        .map(|take| solve_game_take(take))
+        .all(|x| x);
+
+    match is_valid_game {
+        true => game_value,
+        false => 0,
+    }
+}
+
+pub fn solve_puzzle1(input: &str) -> i32 {
+    input.lines().map(|line| solve_game_line(line)).sum()
+}
+
+pub fn solve_puzzle2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| get_largests_colors(line))
+        .fold(0, |acc, f| acc + calculate_power(f))
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solver() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        assert_eq!(solve_game_line(input), 1);
+
+        let input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+
+        // should return 0 because the game is invalid (20 red)
+        assert_eq!(solve_game_line(input), 0);
+
+        let input = "Game 23: 8 green, 6 blue, 9 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+        assert_eq!(solve_game_line(input), 23);
+    }
+
+    #[test]
+    fn test_is_valid_take() {
+        let take = "4 blue";
+
+        assert!(is_valid_take(take));
+        assert_eq!(is_valid_take("24 green"), false);
+    }
+
+    #[test]
+    fn test_solve_puzzle1() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        assert_eq!(solve_puzzle1(input), 8)
+    }
+
+    #[test]
+    fn test_get_largest_colors() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        assert_eq!(get_largests_colors(input), [4, 2, 6]);
+    }
+
+    #[test]
+    fn test_get_colors_values_from_take() {
+        assert_eq!(get_colors_values_from_take("3 blue, 4 red"), [4, 0, 3]);
+        assert_eq!(get_colors_values_from_take("1 red, 2 green"), [1, 2, 0]);
+    }
+
+    #[test]
+    fn test_solve_puzzle2() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        assert_eq!(solve_puzzle2(input), 2286)
+    }
+}
